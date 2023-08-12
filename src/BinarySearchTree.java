d3b9ec90966d836749a040954240f7ad448c9063
vinayharyan28 <vinayharyan28@gmail.com>
2023-08-12 21:44:12 +0530

    working on trees

---
+++ src/BinarySearchTree.java
@@ -0,0 +1,130 @@
+public class BinarySearchTree {
+    private static class Node{
+        private int value, height;
+        private Node left, right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public BinarySearchTree(){
+
+    }
+
+
+    public int height(Node node){
+        if (node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root==null;
+    }
+
+    public void display(){
+        display(root, "Root Node: ");
+    }
+    private void display(Node node, String details){
+        if (node == null){
+            return;
+        }
+        System.out.println(details+node.value);
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
+    }
+
+    public void insert(int value){
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node){
+        if (node == null){
+            return new Node(value);
+        }
+
+        if (value < node.value){
+            node.left = insert(value, node.left);
+        }else {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right));
+        return node;
+    }
+
+    public boolean balanced(){
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node){
+        if (node==null){
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void populate(int[] numbers){
+        for (int number : numbers) {
+            insert(number);
+        }
+    }
+
+    public void preorder(){
+        preorder(root);
+    }
+
+    public void preorder(Node node){
+        if (node == null){
+            return;
+        }
+
+        System.out.print(node.value + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    public void inorder(){
+        inorder(root);
+    }
+
+    public void inorder(Node node){
+        if (node == null){
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.value + " ");
+        inorder(node.right);
+    }
+
+    public void postorder(){
+        postorder(root);
+    }
+
+    public void postorder(Node node){
+        if (node == null){
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+    public static void main(String[] args){
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        int[] numbers = {10, 11, 2, 3, 5, 28, 34};
+        binarySearchTree.populate(numbers);
+        binarySearchTree.display();
+        System.out.println();
+        binarySearchTree.preorder();
+        System.out.println();
+        binarySearchTree.inorder();
+        System.out.println();
+        binarySearchTree.postorder();
+
+    }
+
+}
---
+++ src/BinaryTree.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class BinaryTree {
+    private Node root;
+    public BinaryTree(){
+
+    }
+    private static class Node{
+        int value;
+        Node left, right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    // Insert element
+    public void populate(Scanner scanner){
+        System.out.println("Enter the root node: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner, root);
+    }
+
+    private void populate(Scanner scanner, Node node){
+        System.out.println("Do you want to enter left of " + node.value);
+        boolean left = scanner.nextBoolean();
+        if (left){
+            System.out.println("Enter the left of the left of " + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner, node.left);
+        }
+
+        System.out.println("Do you want to enter right of " + node.value);
+        boolean right = scanner.nextBoolean();
+        if (right){
+            System.out.println("Enter the right of " + node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner, node.right);
+        }
+    }
+
+    public void display(){
+        display(root, 0);
+    }
+
+    private void display(Node node, int level){
+        if (node==null){
+            return;
+        }
+
+        display(node.right, level+1);
+
+        if (level != 0){
+            for (int i=0; i<level-1; i++){
+                System.out.print("|\t\t");
+            }
+            System.out.println("|-------->" + node.value);
+        }else {
+            System.out.println(node.value);
+        }
+        display(node.left, level+1);
+    }
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        BinaryTree tree = new BinaryTree();
+        tree.populate(scanner);
+        tree.display();
+    }
+}
