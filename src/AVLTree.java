4df06a827bff8249ed5cc8550cfbc22614c0b6e6
vinayharyan28 <vinayharyan28@gmail.com>
2023-08-15 15:05:17 +0530

    trees work in progress

---
+++ src/AVLTree.java
@@ -0,0 +1,130 @@
+public class AVLTree {
+    private static class Node{
+        private int value, height;
+        private Node left, right;
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public int height(Node node){
+        if (node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public void insert(int value){
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node){
+        if (node == null){
+            return new Node(value);
+        }
+
+        if (value < node.value){
+            node.left = insert(value, node.left);
+        }else {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right));
+
+        return rotate(node);
+    }
+
+    public Node rotate(Node node){
+        if (height(node.left) - height(node.right) > 1){
+            //left heavy
+            if (height(node.left.left) - height(node.left.right) > 0){
+                // left of left case
+                return rightRotate(node);
+            }
+            if (height(node.left.left) - height(node.left.right) < 0){
+                // left right case
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+
+        if (height(node.left) - height(node.right) < -1){
+            // Right heavy
+            if (height(node.right.left) - height(node.right.right) < 0){
+                // Right of Right case
+                return leftRotate(node);
+            }
+
+            if (height(node.right.left) - height(node.right.right) > 0){
+                // Left right case
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+
+    public Node rightRotate(Node parent){
+        Node child = parent.left;
+        Node terminal = child.right;
+        child.right = parent;
+        parent.left = terminal;
+        parent.height = Math.max(height(parent.left), height(parent.right) + 1);
+        child.height = Math.max(height(child.left), height(child.right) + 1);
+        return child;
+    }
+
+    public Node leftRotate(Node child){
+        Node parent = child.right;
+        Node terminal = parent.left;
+        parent.left = child;
+        child.right = terminal;
+        parent.height = Math.max(height(parent.left), height(parent.right) + 1);
+        child.height = Math.max(height(child.left), height(child.right) + 1);
+        return parent;
+    }
+
+    public void preorder(){
+        preorder(root);
+    }
+
+    public void preorder(Node node){
+        if (node == null){
+            return;
+        }
+
+        System.out.print(node.value + " ");
+        preorder(node.left);
+        preorder(node.right);
+    }
+
+    public void inorder(){
+        inorder(root);
+    }
+
+    public void inorder(Node node){
+        if (node == null){
+            return;
+        }
+        inorder(node.left);
+        System.out.print(node.value + " ");
+        inorder(node.right);
+    }
+
+    public void postorder(){
+        postorder(root);
+    }
+
+    public void postorder(Node node){
+        if (node == null){
+            return;
+        }
+        postorder(node.left);
+        postorder(node.right);
+        System.out.print(node.value + " ");
+    }
+
+
+}
---
+++ src/SegmentTree.java
@@ -0,0 +1,103 @@
+public class SegmentTree {
+    private static class Node {
+        int data, startInterval, endInterval;
+        Node left, right;
+        public Node(int startInterval, int endInterval){
+            this.startInterval = startInterval;
+            this.endInterval = endInterval;
+        }
+    }
+    
+    Node root;
+    
+    public SegmentTree(int[] arr){
+        this.root = constructTree(arr, 0, arr.length-1);
+    }
+
+    private Node constructTree(int[] arr, int start, int end) {
+        if (start==end){
+            //Leaf node
+            Node leaf = new Node(start, end);
+            leaf.data = arr[start];
+            return leaf;
+        }
+
+        // Create new node with index you are at
+        Node node = new Node(start, end);
+        int mid = (start+end)/2;
+        node.left = constructTree(arr, start, mid);
+        node.right = constructTree(arr, mid+1, end);
+        node.data = node.left.data + node.right.data;
+        return node;
+    }
+
+    public void display(){
+        display(root);
+    }
+
+    private void display(Node node) {
+        String str = "";
+        if (node.left != null) {
+            str = str + "Interval=[" + node.left.startInterval + "-" + node.left.endInterval + "] and data: " + node.left.data + " --> ";
+        } else {
+            str = str + "No left child";
+        }
+
+        //for current node
+        str = str + "Interval=[ " + node.startInterval + "-" + node.endInterval + "] and data: "+node.data + " --> ";
+
+        if (node.right != null){
+            str = str + "Interval=[" + node.right.startInterval + "-" + node.right.endInterval + "] and data: " + node.right.data + " --> ";
+        }else {
+            str = str + "No Right child";
+        }
+        System.out.println(str);
+
+        // call recursion
+        if(node.left != null){
+            display(node.left);
+        }
+
+        if (node.right != null){
+            display(node.right);
+        }
+    }
+
+    public int query(int queryStartIndex, int queryEndIndex){
+        return query(root, queryStartIndex, queryEndIndex);
+    }
+
+    private int query(Node node, int queryStartIndex, int queryEndIndex){
+        if(node.startInterval >= queryStartIndex && node.endInterval <= queryEndIndex){
+            //Node is completely lying inside query
+            return node.data;
+        } else if (node.startInterval > queryEndIndex || node.endInterval < queryStartIndex) {
+            return 0;
+        } else{
+            return query(node.left, queryStartIndex, queryEndIndex) + query(node.right, queryStartIndex, queryEndIndex);
+        }
+    }
+
+    // update
+    public int update(Node node, int index, int value){
+        if (index >= node.startInterval && index <= node.endInterval){
+            if (index == node.startInterval && index == node.endInterval){
+                node.data = value;
+            }else {
+                int leftAns = update(node.left, index, value);
+                int rightAns = update(node.right, index, value);
+                node.data = leftAns + rightAns;
+            }
+            return node.data;
+        }
+        return node.data;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 8, 6, 7, -2, -8, 4, 9};
+        SegmentTree segmentTree = new SegmentTree(arr);
+        segmentTree.display();
+        System.out.println(segmentTree.query(1, 6));
+
+    }
+}
